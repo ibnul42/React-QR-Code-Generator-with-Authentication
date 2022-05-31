d1f393bfdb32fb09dfb4980493e8a12a7c0f6008
Ibnul Ashir <ibnulashir42@gmail.com>
2022-05-31 20:27:20 +0600

    login registration

--- src/App.js
+++ src/App.js
@@ -1,76 +1,17 @@
-import {
-  Container,
-  Card,
-  CardContent,
-  Grid,
-  TextField,
-  Button,
-} from "@mui/material";
-
-import QRCode from "qrcode";
-import { useState } from "react";
+import Login from "./Components/Login";
+import Home from "./Components/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [text, setText] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
-  // const classes = useStyles();
-
-  const generateQrCode = async () => {
-    try {
-      const response = await QRCode.toDataURL(text);
-      setImageUrl(response);
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-  };
   return (
-    <Container className="container">
-      <Card>
-        <h2 className="title">Generate Download & scan code</h2>
-        <CardContent>
-          <Grid container spacing={2}>
-            <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
-              <TextField
-                label="Enter Text"
-                onChange={(e) => setText(e.target.value)}
-              />
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={() => generateQrCode()}
-              >
-                Generate
-              </Button>
-              <br />
-              <br />
-              {imageUrl && (
-                <a href={imageUrl} download>
-                  <img src={imageUrl} alt="imageUrl" />
-                </a>
-              )}
-            </Grid>
-            <Grid item xl={4} lg={4} md={6} sm={12} xs={12}></Grid>
-            <Grid item xl={4} lg={4} md={6} sm={12} xs={12}></Grid>
-          </Grid>
-        </CardContent>
-      </Card>
-    </Container>
+    <div>
+      <h2 className="title">Qr Code authentication with React</h2>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
   );
 }
-
-// const useStyles = makeStyles((theme) => ({
-//   container: {
-//     marginTop: 10,
-//   },
-//   title: {
-//     display: "flex",
-//     justifyContent: "center",
-//     alignItems: "center",
-//     backgroundColor: "#3f51b5",
-//     color: "#fff",
-//     padding: 20,
-//   },
-// }));
 
 export default App;
---
+++ src/Components/Login.jsx
@@ -0,0 +1,120 @@
+import { CardContent, Grid, TextField, Button } from "@mui/material";
+
+import { useState } from "react";
+import QRCode from "qrcode";
+import { useNavigate } from "react-router-dom";
+
+//   loginInformation
+const loginInfo = {
+  userName: "aaa",
+  password: "123",
+};
+
+function Login() {
+  const navigate = useNavigate();
+  const [text, setText] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [qrCodeNumber, setQrCodeNumber] = useState("");
+  const [loginMessage, setLoginmessage] = useState(null);
+  const [authenticate, setAuthenticate] = useState(false);
+
+  const [formData, setFormData] = useState({
+    userName: "",
+    password: "",
+  });
+
+  const { userName, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevData) => ({
+      ...prevData,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(loginInfo, formData);
+    if (
+      formData.userName === loginInfo.userName &&
+      formData.password === loginInfo.password
+    ) {
+      setAuthenticate(true);
+      generateQrCode();
+      setLoginmessage("Please verify via QRCode");
+    } else {
+      setLoginmessage("Invalid Credential");
+    }
+  };
+
+  const generateQrCode = async () => {
+    const randomNumber = (Math.random() * 100000).toFixed(0);
+    console.log(randomNumber);
+    try {
+      setQrCodeNumber(randomNumber.toString());
+      const response = await QRCode.toDataURL(randomNumber.toString());
+      setImageUrl(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const verifyQrCode = (text) => {
+    if (qrCodeNumber === text) {
+      navigate("/");
+    } else {
+      alert("Please enter valid QR Code");
+    }
+  };
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="userName"
+          name="userName"
+          value={userName}
+          onChange={onChange}
+        />
+        <br /> <br />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          value={password}
+          onChange={onChange}
+        />
+        <br /> <br />
+        <input type="submit" value="Login" />
+      </form>
+      <p>{loginMessage}</p>
+      {authenticate && (
+        <CardContent>
+          <Grid container spacing={2}>
+            <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+              {imageUrl && (
+                <a href={imageUrl} download>
+                  <img src={imageUrl} alt="imageUrl" />
+                </a>
+              )}
+              <br />
+              <TextField
+                label="Enter Text"
+                onChange={(e) => setText(e.target.value)}
+              />
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={() => verifyQrCode(text)}
+              >
+                Verify
+              </Button>
+            </Grid>
+          </Grid>
+        </CardContent>
+      )}
+    </>
+  );
+}
+
+export default Login;
